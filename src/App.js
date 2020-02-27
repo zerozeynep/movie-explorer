ffcc04d2f199da5341cfe29b1ecd9adc990a1c39
naderalfakesh <nader.alfakesh@gmail.com>
2020-02-27 20:04:37 +0300

    Added Moives List Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import movies from './data';
+import MoviesList from "./MoviesList"
 
 export default class App extends React.Component {
   render() {
-    console.log(movies.results);
+    // console.log(movies.results);
     return (
       <div className="App">
-        <header className="App-header">
-
-        </header>
+        <MoviesList list={movies.results} />
+        
       </div>
     );
   }
---
+++ src/MoviesList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class MoviesList extends Component {
+    render() {
+        const baseURL= "http://image.tmdb.org/t/p/w1280/"
+        // console.log(this.props.list) 
+    const list = this.props.list.map(movie => 
+    <div key={movie.id}>
+        <h1>{movie.original_title}</h1>
+        <img src={baseURL + movie.poster_path} width="600" alt="Poster" />
+    </div>
+    )      
+        return <div>
+            {
+                list
+            }
+        </div>
+    }
+}

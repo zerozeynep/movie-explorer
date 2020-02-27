06ea2e9cb63fa303f391ab457f6d6eb4f3ff704c
naderalfakesh <nader.alfakesh@gmail.com>
2020-02-27 20:25:45 +0300

    dummy header component added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import movies from './data';
 import MoviesList from "./MoviesList"
+import Header from "./Header"
 
 export default class App extends React.Component {
   render() {
-    // console.log(movies.results);
     return (
       <div className="App">
+
+        <div className="App-header">
+        <Header />
         <MoviesList list={movies.results} />
+        </div>
         
       </div>
     );
